6cd3cd10a6b9f632432ed84e37daaa798cbb7c0c
Shelby <shelbyrobison14@gmail.com>
2020-10-14 17:36:30 -0400

    added variables and began get weather function based off city. JS is [WIP]

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 $(document).ready(function () {
-  // Variables
-  var cityInput = $("#city-input");
-  var searchBtn = $("#button-addon2");
-  var clearBtn = $("#clear-search");
-  var weatherHistory = $("#weather-history");
-  var APIKey = "905bcc2048ad4a564317e10f1b767d59";
-  let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+  function weatherInput() {
+    var cityInput = $("#city-input");
+    var searchEl = $("#button-addon2");
+    var clearEl = $("#clear-search");
+    var nameEl = $("#city-name");
+    var currentPicEl = $("#current-pic");
+    var currentTempEl = $("#temperature");
+    var currentHumidityEl = $("#humidity");
+    var currentWindEl = $("#wind-speed");
+    var currentUVEl = $("#uv-index");
+    var historyEl = $("#weather-history");
+    //let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+
+    var APIKey = "905bcc2048ad4a564317e10f1b767d59";
+    //  When search button is clicked, read the city name typed by the user
+
+    function getWeather(cityName) {
+      //  Using saved city name, Request weather information from API
+      let queryURL =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        cityName +
+        "&appid=" +
+        APIKey;
+      weatherInput();
+    }
+  }
 });
