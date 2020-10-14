33e8cb72b915c84d8db7b47cd7267f371e7fd2e7
Shelby <shelbyrobison14@gmail.com>
2020-10-14 16:00:20 -0400

    created global variables in JS

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
+  // Variables
   var cityInput = $("#city-input");
   var searchBtn = $("#button-addon2");
   var clearBtn = $("#clear-search");
+  var weatherHistory = $("#weather-history");
+  var APIKey = "905bcc2048ad4a564317e10f1b767d59";
+  let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 });
